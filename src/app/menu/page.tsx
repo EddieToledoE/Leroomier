3a5c483f370a081919ce972b2006ee89205a4f15
EddieToledoE <Teddy_03@outlook.es>
2024-12-11 20:18:02 -0600

    update

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export default function Menu() {
+  const [user, setUser] = useState("");
   const { data: session, status } = useSession();
-  console.log(session);
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      setUser(session.user?.name || "");
+    }
+  }, [status, session]);
+
   return (
     <div>
       <h1>Menu</h1>
+      <h2> Bienvenido {user}</h2>
       <button onClick={() => signOut()}>CERRAR SESIÓN</button>
     </div>
   );
